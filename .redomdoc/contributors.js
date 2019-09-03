2cbebd4ea604777f065822f9e4f2c3dc664b107f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-09-03 10:47:06 +0100

    feat: add prettier in build.

--- .redomdoc/contributors.js
+++ .redomdoc/contributors.js
@@ -1,117 +1,147 @@
-module.exports = [{
-                            html_url: "https://github.com/pakastin",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/1475902?v=4",
-                            login: "pakastin",
-                        },{
-                            html_url: "https://github.com/maciejhirsz",
-                            avatar_url: "https://avatars1.githubusercontent.com/u/1096222?v=4",
-                            login: "maciejhirsz",
-                        },{
-                            html_url: "https://github.com/tomerigal",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/4153972?v=4",
-                            login: "tomerigal",
-                        },{
-                            html_url: "https://github.com/anttikissa",
-                            avatar_url: "https://avatars1.githubusercontent.com/u/141618?v=4",
-                            login: "anttikissa",
-                        },{
-                            html_url: "https://github.com/talmobi",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/1460615?v=4",
-                            login: "talmobi",
-                        },{
-                            html_url: "https://github.com/andrejcremoznik",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/761287?v=4",
-                            login: "andrejcremoznik",
-                        },{
-                            html_url: "https://github.com/bfrengley",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/37278140?v=4",
-                            login: "bfrengley",
-                        },{
-                            html_url: "https://github.com/jscissr",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/4520820?v=4",
-                            login: "jscissr",
-                        },{
-                            html_url: "https://github.com/mrwanashraf",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/9576239?v=4",
-                            login: "mrwanashraf",
-                        },{
-                            html_url: "https://github.com/terkelg",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/2302254?v=4",
-                            login: "terkelg",
-                        },{
-                            html_url: "https://github.com/h-h-h-h",
-                            avatar_url: "https://avatars1.githubusercontent.com/u/13482553?v=4",
-                            login: "h-h-h-h",
-                        },{
-                            html_url: "https://github.com/131",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/13545?v=4",
-                            login: "131",
-                        },{
-                            html_url: "https://github.com/monkeywithacupcake",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/7316730?v=4",
-                            login: "monkeywithacupcake",
-                        },{
-                            html_url: "https://github.com/katywings",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/4012401?v=4",
-                            login: "katywings",
-                        },{
-                            html_url: "https://github.com/RauliL",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/8155018?v=4",
-                            login: "RauliL",
-                        },{
-                            html_url: "https://github.com/fishead",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/874813?v=4",
-                            login: "fishead",
-                        },{
-                            html_url: "https://github.com/bluenote10",
-                            avatar_url: "https://avatars1.githubusercontent.com/u/3620703?v=4",
-                            login: "bluenote10",
-                        },{
-                            html_url: "https://github.com/sonicdoe",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/652793?v=4",
-                            login: "sonicdoe",
-                        },{
-                            html_url: "https://github.com/joona",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/187605?v=4",
-                            login: "joona",
-                        },{
-                            html_url: "https://github.com/justushamalainen",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/1782573?v=4",
-                            login: "justushamalainen",
-                        },{
-                            html_url: "https://github.com/jung-kurt",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/5148839?v=4",
-                            login: "jung-kurt",
-                        },{
-                            html_url: "https://github.com/louisfoster",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/8028567?v=4",
-                            login: "louisfoster",
-                        },{
-                            html_url: "https://github.com/nikcorg",
-                            avatar_url: "https://avatars0.githubusercontent.com/u/816988?v=4",
-                            login: "nikcorg",
-                        },{
-                            html_url: "https://github.com/RyanV",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/1299728?v=4",
-                            login: "RyanV",
-                        },{
-                            html_url: "https://github.com/spapas",
-                            avatar_url: "https://avatars1.githubusercontent.com/u/3911074?v=4",
-                            login: "spapas",
-                        },{
-                            html_url: "https://github.com/trautonen",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/1641063?v=4",
-                            login: "trautonen",
-                        },{
-                            html_url: "https://github.com/gitter-badger",
-                            avatar_url: "https://avatars2.githubusercontent.com/u/8518239?v=4",
-                            login: "gitter-badger",
-                        },{
-                            html_url: "https://github.com/hville",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/1555569?v=4",
-                            login: "hville",
-                        },{
-                            html_url: "https://github.com/ohpyupi",
-                            avatar_url: "https://avatars3.githubusercontent.com/u/17169119?v=4",
-                            login: "ohpyupi",
-                        },]
+module.exports = [
+    {
+        html_url: "https://github.com/pakastin",
+        avatar_url: "https://avatars3.githubusercontent.com/u/1475902?v=4",
+        login: "pakastin",
+    },
+    {
+        html_url: "https://github.com/maciejhirsz",
+        avatar_url: "https://avatars1.githubusercontent.com/u/1096222?v=4",
+        login: "maciejhirsz",
+    },
+    {
+        html_url: "https://github.com/tomerigal",
+        avatar_url: "https://avatars0.githubusercontent.com/u/4153972?v=4",
+        login: "tomerigal",
+    },
+    {
+        html_url: "https://github.com/anttikissa",
+        avatar_url: "https://avatars1.githubusercontent.com/u/141618?v=4",
+        login: "anttikissa",
+    },
+    {
+        html_url: "https://github.com/talmobi",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1460615?v=4",
+        login: "talmobi",
+    },
+    {
+        html_url: "https://github.com/andrejcremoznik",
+        avatar_url: "https://avatars2.githubusercontent.com/u/761287?v=4",
+        login: "andrejcremoznik",
+    },
+    {
+        html_url: "https://github.com/bfrengley",
+        avatar_url: "https://avatars0.githubusercontent.com/u/37278140?v=4",
+        login: "bfrengley",
+    },
+    {
+        html_url: "https://github.com/jscissr",
+        avatar_url: "https://avatars3.githubusercontent.com/u/4520820?v=4",
+        login: "jscissr",
+    },
+    {
+        html_url: "https://github.com/mrwanashraf",
+        avatar_url: "https://avatars2.githubusercontent.com/u/9576239?v=4",
+        login: "mrwanashraf",
+    },
+    {
+        html_url: "https://github.com/terkelg",
+        avatar_url: "https://avatars2.githubusercontent.com/u/2302254?v=4",
+        login: "terkelg",
+    },
+    {
+        html_url: "https://github.com/h-h-h-h",
+        avatar_url: "https://avatars1.githubusercontent.com/u/13482553?v=4",
+        login: "h-h-h-h",
+    },
+    {
+        html_url: "https://github.com/131",
+        avatar_url: "https://avatars3.githubusercontent.com/u/13545?v=4",
+        login: "131",
+    },
+    {
+        html_url: "https://github.com/monkeywithacupcake",
+        avatar_url: "https://avatars3.githubusercontent.com/u/7316730?v=4",
+        login: "monkeywithacupcake",
+    },
+    {
+        html_url: "https://github.com/katywings",
+        avatar_url: "https://avatars0.githubusercontent.com/u/4012401?v=4",
+        login: "katywings",
+    },
+    {
+        html_url: "https://github.com/RauliL",
+        avatar_url: "https://avatars3.githubusercontent.com/u/8155018?v=4",
+        login: "RauliL",
+    },
+    {
+        html_url: "https://github.com/fishead",
+        avatar_url: "https://avatars2.githubusercontent.com/u/874813?v=4",
+        login: "fishead",
+    },
+    {
+        html_url: "https://github.com/bluenote10",
+        avatar_url: "https://avatars1.githubusercontent.com/u/3620703?v=4",
+        login: "bluenote10",
+    },
+    {
+        html_url: "https://github.com/sonicdoe",
+        avatar_url: "https://avatars0.githubusercontent.com/u/652793?v=4",
+        login: "sonicdoe",
+    },
+    {
+        html_url: "https://github.com/joona",
+        avatar_url: "https://avatars2.githubusercontent.com/u/187605?v=4",
+        login: "joona",
+    },
+    {
+        html_url: "https://github.com/justushamalainen",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1782573?v=4",
+        login: "justushamalainen",
+    },
+    {
+        html_url: "https://github.com/jung-kurt",
+        avatar_url: "https://avatars2.githubusercontent.com/u/5148839?v=4",
+        login: "jung-kurt",
+    },
+    {
+        html_url: "https://github.com/louisfoster",
+        avatar_url: "https://avatars3.githubusercontent.com/u/8028567?v=4",
+        login: "louisfoster",
+    },
+    {
+        html_url: "https://github.com/nikcorg",
+        avatar_url: "https://avatars0.githubusercontent.com/u/816988?v=4",
+        login: "nikcorg",
+    },
+    {
+        html_url: "https://github.com/RyanV",
+        avatar_url: "https://avatars2.githubusercontent.com/u/1299728?v=4",
+        login: "RyanV",
+    },
+    {
+        html_url: "https://github.com/spapas",
+        avatar_url: "https://avatars1.githubusercontent.com/u/3911074?v=4",
+        login: "spapas",
+    },
+    {
+        html_url: "https://github.com/trautonen",
+        avatar_url: "https://avatars3.githubusercontent.com/u/1641063?v=4",
+        login: "trautonen",
+    },
+    {
+        html_url: "https://github.com/gitter-badger",
+        avatar_url: "https://avatars2.githubusercontent.com/u/8518239?v=4",
+        login: "gitter-badger",
+    },
+    {
+        html_url: "https://github.com/hville",
+        avatar_url: "https://avatars3.githubusercontent.com/u/1555569?v=4",
+        login: "hville",
+    },
+    {
+        html_url: "https://github.com/ohpyupi",
+        avatar_url: "https://avatars3.githubusercontent.com/u/17169119?v=4",
+        login: "ohpyupi",
+    },
+];
