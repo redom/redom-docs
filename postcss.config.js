7568f1ba86c154d4e9982ed30fa8fb0cb602614f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-09-03 11:16:07 +0100

    fix: minify file.

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,6 @@ module.exports = {
     plugins: [
         require("tailwindcss"),
         require("autoprefixer"),
-        require("cssnano")({
-            preset: "default",
-        })
+        require("cssnano")
     ],
 };
